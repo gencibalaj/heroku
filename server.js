3155bb5d3e548063e3e025f6bb660412fcbeba7d
Genc <genc.balaj@elba-tech.com>
2022-07-12 11:52:07 +0200

    bug

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.get('/',function(req,res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(3030, ()=>{console.log('server is listening on port 3030')})
+app.listen(process.env.PORT || 3030, ()=>{console.log('server is listening')})
